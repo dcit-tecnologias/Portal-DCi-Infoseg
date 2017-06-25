0c578029cd1a9d30826fef9e3faa7965fe1827ce
Dan <Dan@MESA1>
2017-06-24 23:31:31 -0300

    Add project files.

---
+++ Portal DCi Infoseg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Portal_DCi_Infoseg.MvcApplication" Language="C#" %>
---
+++ Portal DCi Infoseg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Portal_DCi_Infoseg.Startup))]
+namespace Portal_DCi_Infoseg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
